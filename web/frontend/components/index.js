832339bae9a5ebe0ea97cdea0dfa595769cb9ac8
uteej10 <cg@prpwebs.com>
2022-12-05 19:24:54 +0530

    support and add store3

--- web/frontend/components/index.js
+++ web/frontend/components/index.js
@@ -3,14 +3,10 @@ export { StoreLists } from "./StoreLists";
 export { StoreSettings } from './StoreSettings';
 export { DropZoneUpload } from './DropZoneUpload';
 export { Instructions } from './Instructions';
-<<<<<<< HEAD
 export { Support } from './Support';
 export { AddStore } from './AddStore';
 
 
-=======
-export { ImportExport } from './ImportExport';
->>>>>>> a030dda54024fec77547fa00dc5c620abc9ae1b6
 
 export * from "./providers";
 
